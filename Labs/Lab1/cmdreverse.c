318c193e96e7660befa153ba88248e49a58bc933
ebs021 <ebs021@bucknell.edu>
2015-09-01 11:05:09 -0400

    Pre-lab 1.2 completed

--- Labs/Lab1/cmdreverse.c
+++ Labs/Lab1/cmdreverse.c
@@ -5,33 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* main(int argc, char *str)
+char* main(int argc, char *argv[])
 {
 
-char temp;
-size_t len = strlen(str) -1;
-size_t i;
-size_t k = len;
+char *string = malloc(strlen(argv[1]+1));
+ strcpy(string, argv[1]);
+ int len = strlen(argv[1]);
 
-	if (argc == 2) {
 
-	for(i=0; i<len; i++)
-		{
-			temp = str[k];
-			str[k] = str[i];
-			str[i] = temp;
-			k--;
+ if (argc == 2) {    //if there is on parameter passed into the function
+   while (len>=0){
+         printf("%c", string[len]);
+          len --;
+    }
+   printf("\n");
 
-		printf("%s\n",str);
-
-		}
-	}
-
-	else{
-		printf("print2");
-		exit(0);
-
-	}
+ } else{
+   printf("Failed\n");
+   }
 }
 
 
