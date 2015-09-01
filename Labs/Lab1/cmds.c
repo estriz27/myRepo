c90be17fdd6870adb4b0fdf9cf685ab0cf89bfd7
ebs021 <ebs021@bucknell.edu>
2015-09-01 10:58:07 -0400

    Pre-lab 1.1 completed

--- Labs/Lab1/cmds.c
+++ Labs/Lab1/cmds.c
@@ -3,29 +3,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	char first;
 	int second;
 	float third;
-	char fourth[20];
+	char *fourth = malloc(strlen(argv[4]+1));
 
 //check if 4 parameters are provided
 
 if (argc == 5) {
 
 	first = *argv[1];
-	second = *argv[2];
-	third = *argv[3];
-	strcpy(fourth, *argv[4]);
+	second = atoi(argv[2]);
+	third = strtof(argv[3],NULL);
+	strcpy(fourth, argv[4]);
 	//else exit
 	
-	printf("%c\n%d\%f.2\n%s\n",first,second,third,fourth);
+	printf("%c\n%d\n%f\n%s\n",first,second,third,fourth);
 
 }
 else{
-	printf("%s", "failure");
+	printf("%s", "failure\n");
 exit(0);
 
 
