4525234cfa9a5a0dcaeb5e7f97d668e9b797c4bb
ebs021 <ebs021@bucknell.edu>
2015-09-13 20:29:18 -0400

    Lab 2.2 completed

---
+++ Labs/Lab2/upper.c
@@ -0,0 +1,143 @@
+//Elias Strizower
+//CSCI315
+//Lab 2 Prelab 
+
+
+
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 12
+#define READ_END 0 
+#define WRITE_END 1
+
+int Fork(void){
+  pid_t pid;
+  if((pid = fork()) == -1){
+    perror("Error creating child process");
+    exit(-1);
+  }
+  else{
+    return pid;
+  }
+}
+
+int Pipe(int pipefd[2]){
+  if (pipe(pipefd) < 0){
+    perror("Error creating pipe");
+    exit(-1);
+  }
+  return(0);
+}
+
+
+int Read(int fd, void *buf, size_t count){
+ int resultR =  read(fd, buf, count);
+  if(resultR < 0){
+    perror("Error reading from pipe");
+    exit(-1);
+  }
+  else{
+    return resultR;
+  }
+}
+  
+  int Write(int fd, const void *buf, size_t count){
+    int resultW = write(fd, buf, count);
+    if(resultW <0){
+      perror("Error writing to pipe");
+    }
+    else{
+      return resultW;
+  }
+  }
+
+int main(void)
+{
+  char write_msg[BUFFER_SIZE] = "\nGreetings\n";
+  char read_msg[BUFFER_SIZE];
+  char write_msg2[BUFFER_SIZE] = "\nHeyThere!\n";
+  char read_msg2[BUFFER_SIZE];
+  
+  int fd[2];
+  int fd2[2];
+  pid_t pid;
+  int x = 0;
+  int y = 0;
+  int a = 0;
+  int b = 0;
+
+  
+  /* create the pipe */
+  Pipe(fd);
+  Pipe(fd2);
+  
+  /* fork a child process */
+  pid = Fork();
+  
+  
+  if (pid > 0) { /* parent process */
+    /* close the unused end of the pipe */
+    close(fd[READ_END]);
+    close(fd2[WRITE_END]);
+  
+    /* write to the pipe */
+    
+    while (write_msg[x] != 0){
+    Write(fd[WRITE_END],(void*) &write_msg[x], 1);
+    // write(fd[WRITE_END],"\n" , 1);    //test to see if writing 1 char at a time
+    x++;
+    }
+    /* close the write end of the pipe */
+    close(fd[WRITE_END]);
+  
+  //Have parent read from second pipe
+  while(1){
+    Read(fd2[READ_END], (void*) &read_msg2[a],1);
+	 printf("read2 %c \n", read_msg2[a]);
+	 if (read_msg2[a] == 0){
+	   break;
+	 }
+	 a++;
+       }
+  //Close the read end of the pipe
+     close(fd2[READ_END]);
+  }
+
+
+ else { /* child process */
+   /* close the unused end of the pipe */
+   close(fd[WRITE_END]);
+   close(fd2[READ_END]);
+   /* read from the pipe */
+   while(1){
+     Read(fd[READ_END],(void*) &read_msg[y], 1);
+     read_msg[y] = toupper(read_msg[y]);
+     printf("read %c \n",read_msg[y]);
+     
+     if(read_msg[y] == 0){
+       break;
+     }
+     y++;
+   }
+   /* close the write end of the pipe */
+   close(fd[READ_END]);
+   
+    while (write_msg2[b] != 0){
+    Write(fd2[WRITE_END],(void*) &write_msg2[b], 1);
+    // write(fd[WRITE_END],"\n" , 1);    //test to see if writing 1 char at a time
+    b++;
+    }
+    /* close the write end of the pipe */
+    close(fd2[WRITE_END]);
+ }
+   
+}
+ 
+
+
