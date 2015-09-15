ab29e699bd13ed79ffd840fd753a30af055efd75
ebs021 <ebs021@bucknell.edu>
2015-09-15 10:06:55 -0400

    Pre-lab 3.2 completed (no seg fault)

--- Labs/Lab3/mytime.c
+++ Labs/Lab3/mytime.c
@@ -2,14 +2,15 @@
 //Lab 3 prelab
 
 #include <sys/time.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]){
   struct timeval myTime;
     gettimeofday(&myTime, NULL);
-  const time_t timeStr = myTime.tv_sec;
-  printf("%s\n", ctime(&timeStr));
+  time_t timeStr = myTime.tv_sec;
+  printf("%s", ctime(&timeStr));
 
 
 }
