a3d65cbf8e1bcc5aa793cc56f29bbaf99a181835
ebs021 <ebs021@bucknell.edu>
2015-10-06 11:11:17 -0400

    Lab 5 completed

--- Labs/Lab5/include/circular-list.h
+++ Labs/Lab5/include/circular-list.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2013 Bucknell University
  *
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
